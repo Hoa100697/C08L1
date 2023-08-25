7f9a2745fde087d7a64c36c86008a693bb828b48
Your Name <nguyenhoa100697@gmail.com>
2023-08-25 08:08:12 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //JDK: java development kit
+        //JRE: Javar runtiomr enviroment
+        //JVM: Java vitual machine
+        //KDL bien = gtri
+        int number = 1;
+        byte numberbyte = 1;
+        float numberFloat = 1.1f;
+        double numberdouble = 1.2;
+        boolean booleanresult = true;
+        System.out.println(number);
+        System.out.println(numberbyte);
+        System.out.println(numberFloat);
+        System.out.println(numberdouble);
+        System.out.println(booleanresult);
+    }
+}
